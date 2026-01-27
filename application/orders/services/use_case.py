70d86d559b352207f565b0040c48473b285c7e7a
Gadzimurad Umarov <gadzimuradumarov0410@gmail.com>
2026-01-27 14:33:09 +0300

    Add documentation to Use Case.

--- application/orders/services/use_case.py
+++ application/orders/services/use_case.py
@@ -1,4 +1,13 @@
-﻿from application.orders.integrations.google_sheets.repository import SheetsRepository
+﻿"""
+    Это Use Case слой, который оркестрирует сценарии бизнес-логики проекта:
+        1. Создание заказа
+        2. Обновление заказа (статус, дата отгрузки, даты начала и конца заказа)
+        3. Отмена заказа
+    Данный слой независим от контрактов
+"""
+
+
+from application.orders.integrations.google_sheets.repository import SheetsRepository
 from application.orders.integrations.market.client import get_order
 from application.orders.repo import OrderRepository
 from application.orders.services.manage_repo import _create_order_with_items, _dto_to_order
@@ -12,29 +21,28 @@ async def handle_order_created(
         repo: SheetsRepository,
 ):
     try:
-        order_from_market = await get_order(notification.posting_number)
+        order_from_market = await get_order(notification.posting_number)    # Получение заказа с озона по его номеру из вебхука
         orders_from_db = \
-            await OrderRepository.get_order_items_by_posting_number(notification.posting_number)
+            await OrderRepository.get_order_items_by_posting_number(notification.posting_number)    # Получение записи заказа из бд по его номеру из вебхука
 
-        if len(orders_from_db) == 0:
+        if len(orders_from_db) == 0:    # Проверка на наличие записи о заказе в бд, если записей нет, то программа записывает
             order_items_dto = await _transforming_order_creation_data(
                 order_data=order_from_market
-            )
+            )   # Преобразование данных полученных из get_order в pydantic схему, которую можно преобразовать в ORM модель
 
-            dto_to_model = [_dto_to_order(dto) for dto in order_items_dto]
-            print(order_items_dto, notification, order_from_market)
+            dto_to_model = [_dto_to_order(dto) for dto in order_items_dto]  # Преобразование pydantic схемы в ORM модель
 
-            orders = await _create_order_with_items(dto_to_model)
+            orders = await _create_order_with_items(dto_to_model)   # Создание записи в бд
 
-            if not orders:
+            if not orders:  # Проверка того созданы ли записи в бд
                 return {"message": "Order creation in database failed"}
 
-        order_items_to_sheet = await _create_order_items_in_sheets(repo, order_items_dto)
+        order_items_to_sheet = await _create_order_items_in_sheets(repo, order_items_dto)   # Создание записи о заказе в таблице
 
-        if not order_items_to_sheet:
+        if not order_items_to_sheet:    # Проверка наличия записи в таблице
             return {"message": "Order creation in sheet failed"}
 
-    except Exception as e:
+    except Exception as e:  # Отлов ошибок
         return {"message": "Неизвестная ошибка", "error": str(e)}
 
     return {"message": "Ok"}
